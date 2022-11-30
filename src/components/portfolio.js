57ce458dcd2d7b2d66a2d92188806c40428b2a58
Kashish <kashish.biserwal@protonmail.com>
2022-11-30 22:32:22 +0530

    changes

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -11,6 +11,27 @@ export default function Portfolio(){
             <div class="card">
                 <div class="face face1">
                     <div class="project-content">
+                        <img src="images/figurz.jpeg"/>
+                    </div>
+                    <div class="pb">
+                        <button class="phone-buttons"><i class="fa-brands fa-github"></i><a href="https://github.com/KashishBiserwal/figurz">Repo</a></button>
+                        <button class="phone-buttons"><a href="https://superheromerch.netlify.app/">Demo</a></button>
+                    </div>
+                </div>
+                <div class="face face2">
+                    <div class="project-content">
+                        <h2>Figurz</h2>
+                        <br/>
+                        <p class="content-p">An eCommerce website for superheroes merchandise.</p>
+                        <br/>
+                        <button><i class="fa-brands fa-github"></i><a href="https://github.com/KashishBiserwal/figurz">Repository</a></button>
+                        <button><a href="https://superheromerch.netlify.app/">Demo</a></button>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="face face1">
+                    <div class="project-content">
                         <img src="images/mymeal.png"/>
                     </div>
                     <div class="pb">
@@ -58,7 +79,7 @@ export default function Portfolio(){
                         <img src="images/rpsls.png"/>
                     </div>
                     <div class="pb">
-                        <button class="phone-buttons"><i class="fa-brands fa-github"></i> <a href="https://kashishbiserwal.github.io/rock-paper-scissors-lizard-spock/">Repo</a></button>
+                        <button class="phone-buttons"><i class="fa-brands fa-github"></i> <a href="https://github.com/KashishBiserwal/rock-paper-scissors-lizard-spock">Repo</a></button>
                         <button class="phone-buttons"><a href="https://kashishbiserwal.github.io/rock-paper-scissors-lizard-spock/">Demo</a></button>
                     </div>
                 </div>
@@ -69,7 +90,7 @@ export default function Portfolio(){
                         <p class="content-p">Take rock paper scissors to the next level.</p>
                         <br/>
                         <div>
-                            <button><i class="fa-brands fa-github"></i> <a href="https://kashishbiserwal.github.io/rock-paper-scissors-lizard-spock/">Repository</a></button>
+                            <button><i class="fa-brands fa-github"></i> <a href="https://github.com/KashishBiserwal/rock-paper-scissors-lizard-spock">Repository</a></button>
                             <button><a href="https://kashishbiserwal.github.io/rock-paper-scissors-lizard-spock/">Demo</a></button>
                         </div>
                     </div>
